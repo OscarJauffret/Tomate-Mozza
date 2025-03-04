b58f903302db2b97c0086ed94471cc9f4cc9cdf0
JauffretOscar <oscar.jauffret@ulb.be>
2025-03-04 14:49:55 +0100

    Automatically launching the map, although need to simulate a click in the window

--- disco/multiple_clients.py
+++ disco/multiple_clients.py
@@ -1,3 +1,4 @@
+import os
 import time
 from time import sleep
 from tminterface.client import Client, run_client
@@ -6,8 +7,13 @@ import multiprocessing as mp
 import numpy as np
 import subprocess
 from utils import *
-import pyautogui
+import threading
+import pywinctl
+import psutil
 
+registered_clients = 0
+num_clients = 2
+lock = threading.Lock()
 
 class MainClient(Client):
     def __init__(self, num) -> None:
@@ -17,15 +23,20 @@ class MainClient(Client):
 
     def on_registered(self, iface: TMInterface) -> None:
         print(f"Registered to {iface.server_name}")
+
+        timer = threading.Timer(5, self.launch_map, args=(iface,))
+        timer.start()
+
+    def launch_map(self, iface: TMInterface) -> None:
         iface.execute_command(f"map {get_default_map()}")
 
     def on_run_step(self, iface: TMInterface, _time: int) -> None:
         if _time >= 0:
             state = iface.get_simulation_state()
             if self.num == 0:
-                iface.execute_command(f"press up; steer {np.sin(_time / 1000) * 65535}")
+                iface.execute_command(f"press up")
             else:
-                iface.execute_command(f"press down; steer {np.sin(_time / 1000) * 65535}")
+                iface.execute_command(f"press down")
             self.prev_state = state
 
     def on_checkpoint_count_changed(self, iface: TMInterface, current: int, target: int):
@@ -35,22 +46,40 @@ class MainClient(Client):
         iface.execute_command(f"log {msg}")
 
 
+def launch_games():
+    for i in range(num_clients):
+        launch_game()
+        sleep(2)
+
 def launch_game():
     executable, path_to_executable = get_executable_path()
-    result = subprocess.run([executable, "-PassThru"], cwd=path_to_executable, shell=True, text=True)
-    print(result.stdout)
+    subprocess.Popen([executable], cwd=path_to_executable, shell=True)
 
 def worker(server_num):
-    sleep(server_num*2)
-    launch_game()
+    sleep(2*server_num)
     server_name = f"TMInterface{server_num}"
     print(f"Connecting to {server_name}...")
     client = MainClient(server_num)
     run_client(client, server_name)
 
-
 if __name__ == "__main__":
-    num_clients = 1
+    launch_games()
+    pids = get_process_pids_by_name("TmForever.exe")
+    move_windows(pids)
+
     servers = [i for i in range(num_clients)]
-    with mp.Pool(num_clients) as pool:
-        pool.map(worker, servers)
+
+    # Create processes
+    processes = []
+    for server in servers:
+        process = mp.Process(target=worker, args=(server,))
+        processes.append(process)
+        process.start()
+
+    sleep(3)
+    focus_windows_by_pids(pids)
+    print("Windows focused")
+
+    # Wait for all processes to finish
+    for process in processes:
+        process.join()
--- disco/utils.py
+++ disco/utils.py
@@ -1,5 +1,10 @@
 from dotenv import load_dotenv
+import psutil
+import pywinctl
 import os
+import win32gui, win32process, win32con, win32com.client
+from time import sleep
+import pyautogui
 
 load_dotenv()
 
@@ -8,3 +13,60 @@ def get_executable_path() -> tuple[str, str]:
 
 def get_default_map() -> str:
     return os.path.join(os.getenv("DEFAULT_MAP_PATH"), os.getenv("DEFAULT_MAP_NAME"))
+
+def get_process_pids_by_name(process_name="TMForever.exe"):
+    pids = []
+    for process in psutil.process_iter(attrs=['pid', 'name']):
+        if process.info['name'] and process_name.lower() in process.info['name'].lower():
+            pids.append(process.info['pid'])
+
+    return pids
+
+def get_hwnds_for_pid(pid):
+    def callback(hwnd, hwnds):
+        #if win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
+        _, found_pid = win32process.GetWindowThreadProcessId(hwnd)
+
+        if found_pid == pid:
+            hwnds.append(hwnd)
+        return True
+    hwnds = []      # list to store handles of windows
+    win32gui.EnumWindows(callback, hwnds)
+    return hwnds
+
+def focus_window(hwnd):
+    if hwnd:
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shell.SendKeys('%')
+        win32gui.SetForegroundWindow(hwnd)
+        sleep(0.1)
+        rect = win32gui.GetWindowRect(hwnd)
+        x = rect[0] + 50
+        y = rect[1] + 50
+        pyautogui.click(x, y)
+    else:
+        print("Window not found")
+
+def focus_window_by_pid(pid):
+    hwnds = get_hwnds_for_pid(pid)
+    for hwnd in hwnds:
+        focus_window(hwnd)
+
+def focus_windows_by_pids(pids):
+    for pid in pids:
+        focus_window_by_pid(pid)
+        sleep(3)
+
+def move_windows(pids):
+    w, h = 640, 480
+
+    windows_horizontally = 1920 // w
+    windows_vertically = 1080 // h
+    for i, pid in enumerate(pids):
+        x = (i % windows_horizontally) * w
+        y = (i // windows_horizontally % windows_vertically) * h
+
+        hwnds = get_hwnds_for_pid(pid)
+        for hwnd in hwnds:
+            win32gui.MoveWindow(hwnd, x, y, w, h, True)
+
