6b47e4c218490b6f185bdb3ac1eb6990035ae170
JauffretOscar <oscar.jauffret@ulb.be>
2025-03-11 17:51:25 +0100

    config file

--- src/config.py
+++ src/config.py
@@ -1,9 +1,7 @@
-from torch.distributions import Gamma
-
 NUMBER_OF_CLIENTS: int = 1
 WINDOW_NAME: str = "TrackMania Nations Forever (TMInterface 1.4.3)"
 
-MAP_LAYOUT_PATH: str = "maps/ordered_blocks.txt"
+MAP_LAYOUT_PATH: str = "maps/ordered_blocks.json"
 MAP_GBX_OUTPUT_PATH: str = "maps/horizon_layout.txt"
 
 INPUT_SIZE: int = 6
