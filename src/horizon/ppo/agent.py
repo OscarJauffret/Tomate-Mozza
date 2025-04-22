16e9550a38a474db3a0e816f7e83364bb5bc2e5d
JauffretOscar <oscar.jauffret@ulb.be>
2025-04-22 15:23:17 +0200

    Fixed a bug of len

--- src/horizon/ppo/agent.py
+++ src/horizon/ppo/agent.py
@@ -120,7 +120,7 @@ class PPOAgent(Agent):
     def on_run_step(self, iface: TMInterface, _time: int) -> None:
         if _time == 0:
             if Config.Game.CURRICULUM_LEARNING:
-                self.spawn_point = random.randint(0, len(self.unlocked_states))
+                self.spawn_point = random.randint(0, self.unlocked_states)
                 iface.execute_command(f"load_state {self.random_states[self.spawn_point]}")
             self.ready = True
             if self.save_pb:
